e9161a953cc5cccecaa9e762f4a6496feeaf722c
Saurav Pandey <sp4398@gmail.com>
2025-10-30 19:56:55 +0530

    Add responsive side menu to Navbar component

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import { assets } from "@/assets/assets";
-import React from "react";
+import React, { useRef } from "react";
 
 const Navbar = () => {
+  const sideMenuRef = useRef();
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(-16rem)";
+  };
+
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(16rem)";
+  };
+
   return (
     <>
       <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
@@ -48,7 +57,51 @@ const Navbar = () => {
             Contact
             <Image src={assets.arrow_icon} alt="contact" className="w-3" />
           </a>
+          <button
+            className="block md:hidden ml-3 cursor-pointer"
+            onClick={openMenu}
+          >
+            <Image src={assets.menu_black} alt="menu" className="w-6" />
+          </button>
         </div>
+
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          </div>
+          <li>
+            <a href="#top" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" onClick={closeMenu}>
+              About me
+            </a>
+          </li>
+          <li>
+            <a href="#services" onClick={closeMenu}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#work" onClick={closeMenu}>
+              My Work
+            </a>
+          </li>
+          <li>
+            <a href="#contact" onClick={closeMenu}>
+              Contact me
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
   );
