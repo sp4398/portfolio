ef884e0501906301b3a68a7b1655fecaaf9e45f7
Saurav Pandey <sp4398@gmail.com>
2025-11-04 00:49:07 +0530

    Update Navbar component styles for improved scroll effect and background opacity

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
       </div>
       <nav
         className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50  ${
-          isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""
+          isScroll ? "bg-white/30 backdrop-blur-lg shadow-sm" : ""
         }`}
       >
         <a href="#top">
@@ -42,7 +42,7 @@ const Navbar = () => {
         </a>
         <ul
           className={`hidden md:flex justify-between items-center gap-6 lg:gap-8 rounded-full px-12
-         py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"} `}
+         py-3 ${isScroll ? "" : "bg-white/30 shadow-sm "} `}
         >
           <li>
             <a href="#top">Home</a>
