1d835c0f156aa5f00528057eaf11fdc9ecfd31b5
Saurav Pandey <sp4398@gmail.com>
2025-11-05 21:54:52 +0530

    Add Services component and integrate it into the Home page

---
+++ app/components/Services.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg">What I Offer</h4>
+      <h2 className="text-center text-5xl ">My Services</h2>
+    </div>
+  );
+};
+
+export default Services;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import About from './components/About'
+import Services from './components/Services'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar/>
       <Header/>
       <About/>
+      <Services />
     </div>
   )
 }
